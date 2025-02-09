09e520bdfcd28ba9c48f6eceef98e89e3f5271e1
Ilyasse-B <ilyasseboudjemline@gmail.com>
2025-02-09 20:32:44 +0000

    Merged new app routes by Adi in

--- server/models/ListingsModel.py
+++ server/models/ListingsModel.py
@@ -7,12 +7,14 @@ class ListingsModel(db.Model):
 
 
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, nullable=False, unique = True)
     user_id = db.Column(db.Integer,db.ForeignKey('user_table.id'), nullable=False)
     listing_name = db.Column(db.String(200), nullable=False, unique = True)
     image = db.Column(db.String(500), nullable=False, unique = True)
     price = db.Column(db.Float, nullable=False)
-
+    #condition = db.Column(db.String(200), nullable=False)
+    #category = db.Column(db.String(200), nullable=False)
+    #description = db.Column(db.String(200), nullable=False)
 
 
     def __repr__(self):
